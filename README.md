bd2c75d0607fae50e3e34eef039a25b7fc3dc58b
Bruce Elgort <belgort@clark.edu>
2019-04-15 11:48:31 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ctec-126-lab-4
+# CTEC 126 - Lab No. 4
+
+## Song Lyrics
+
+Using functions, create JavaScript code that will generate lyrics to a song. Your functions will return the HTML and lyrics to a main function. The main function will then concatenate all of the returned output and place it on the web page.
+
+Starter code has been provided for you to use.
+
---
+++ js/lab4.js
@@ -0,0 +1,46 @@
+// lab4.js
+// The verse1(), verse2(), chorus1() and chorus2() are provided to you. You can change the function names and add additonal ones as needed. Be sure to follow the starter code that has been supplied.
+
+function verse1(){
+    let output = '';
+    // your code here
+
+    return output;
+}
+
+function verse2(){
+    let output = '';
+    // your code here
+
+    return output;
+}
+
+function chorus1(){
+    let output = '';
+    // your code here
+
+    return output;
+}
+
+function chorus2(){
+    let output = '';
+    // your code here
+
+    return output;
+}
+
+
+
+function main(){
+    let finalOutput = ''; // used to build lyrics string
+    // call verse and chorus functions
+    // each verse/chorus should return text including HTML paragraph tags
+
+    // do not modify the statement below
+    // it will take 
+    document.getElementById('lyrics').innerHTML = finalOutput;
+}
+
+window.onload = function(){
+    main();
+}
